947dc512a35b02ce0b84ab464a41081a0cbab505
Nick Yaremovich <nickyaremovich@gmail.com>
2026-05-18 18:25:51 -0600

    implement stock picker method

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,3 +1,18 @@
-def stock_picker
-  
+stocks = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(prices)
+  best_profit = 0
+  best_days = []
+  prices.each_with_index do |buy, index|
+    (index + 1...prices.length).each do |sell|
+      profit = prices[sell] - buy
+      if profit > best_profit
+        best_profit = profit
+        best_days = [index, sell]
+      end
+    end
+  end
+  best_days
 end
+
+p stock_picker(stocks)
